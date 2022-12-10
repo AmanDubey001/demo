72fe46d507e6a2d5db5b691ccd3fa7c2cf1f6d53
Aman <Amandubey451@gmail.com>
2022-12-11 01:59:19 +0530

    Assignment

---
+++ src/components/Greet.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+//  export function Greet(){
+//     return <h1>Hello Aman!!!</h1>
+// }
+//export default Greet
+const Greet = () => {
+    return (<h1>
+        Heyy!!Hellooooooo!
+            </h1>)
+} 
+     
+      
+      
+      
+            
+      
+ 
+
+export default Greet
---
+++ src/components/column.js
@@ -0,0 +1,66 @@
+export const COLUMNS=[
+{
+    Header:'id',
+    accessor:'id'
+},
+{
+    Header:'first_name',
+    accessor:'first_name'
+
+},
+{
+    Header:'last_name',
+    accessor:'last_name'
+
+},
+{
+    Header:'email',
+    accessor:'email'
+
+},
+{
+    Header:'gender',
+    accessor:'gender'
+
+},
+{
+    Header:'ip_address',
+    accessor:'ip_address'
+
+},
+{
+    Header:'airport_code',
+    accessor:'airport_code'
+
+},
+{
+    Header:'time',
+    accessor:'time'
+
+},
+{
+    Header:'status',
+    accessor:'status'
+
+},
+{
+    Header:'mobile',
+    accessor:'mobile'
+
+},
+{
+    Header:'area',
+    accessor:'area'
+
+},
+{
+    Header:'show',
+    accessor:'show'
+
+},
+{
+    Header:'edit',
+    accessor:'edit'
+
+},
+]
